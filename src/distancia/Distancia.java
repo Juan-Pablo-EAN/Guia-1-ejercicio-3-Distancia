2cd2961bcf03075b4a750d07f54c63b5fff78c4e
ComunidadColee <coleecomunidad@gmail.com>
2020-07-26 11:02:11 -0500

    ejercicio3

---
+++ src/distancia/Distancia.java
@@ -0,0 +1,33 @@
+package distancia;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Distancia {
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+       DecimalFormat decimales = new DecimalFormat("#.000");
+       double lat1 = (double) 0;
+       double lat2 = (double) 0;
+       double lon1 = (double) 0;
+       double lon2 = (double) 0;
+       String ciudad1 = "";
+       String ciudad2 = "";
+       
+       System.out.println ("Para hallar la distancia entre dos ciudades sigue las instrucciones:");
+       System.out.println("Escribe el nombre de la primera ciudad:");
+       ciudad1 = entrada.nextLine();
+       System.out.println("Escribe el nombre de la segunda ciudad:");
+       ciudad2 = entrada.nextLine();
+       System.out.println ("Escribe la latitud de" + " " + ciudad1 + " " + "en decimales:");
+       lat1 = entrada.nextInt();
+       System.out.println("Escribe la longitud de" + " " + ciudad1 + ":");
+       lon1 = entrada.nextInt();
+       System.out.println ("Ahora escribe la latitud de" + " " + ciudad2 + ":");
+       lat2 = entrada.nextInt();
+       System.out.println("Por ultimo escribe la longitud de" + " " + ciudad2 + ":");
+       lon2 = entrada.nextInt();
+       double distancia = (double)((6371.07*Math.acos(Math.sin(lat1)))*(Math.sin(lat2)+ Math.cos(lat1))*(Math.cos(lat2)*Math.cos(lon1-lon2)));
+       System.out.println("");
+       System.out.println("La distancia entre" + " " + ciudad1 + " " + "y" + " " + ciudad2 + " " + "es:" + " " + decimales.format(distancia) + "km");
+    }
+}
